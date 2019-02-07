a53059edecfc5763901cbb7a8504c297dfcc1084
Dmitry Shapkin <shapkindev@gmail.com>
2019-02-07 17:26:48 +0300

    Second commit

--- FirstPlayground.playground/Contents.swift
+++ FirstPlayground.playground/Contents.swift
@@ -1,6 +1,27 @@
 // First playground
 
+import UIKit
+
 // Thread
 // Operation
 // GCD
 
+// UNIX - POSIX (low level)
+
+var thread = pthread_t(bitPattern: 0) // create the thread
+var attribute = pthread_attr_t()
+
+pthread_attr_init(&attribute)
+pthread_create(&thread, &attribute, { (pointer) -> UnsafeMutableRawPointer? in
+    print("Unix Test")
+    return nil
+}, nil)
+
+// Thread Two (Swift implementation)
+
+var nsThread = Thread {
+    print("Swift Test")
+}
+
+nsThread.start()
+nsThread.cancel()
