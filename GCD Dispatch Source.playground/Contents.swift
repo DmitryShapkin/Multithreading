bf2a6fb8b5a0c9e8fc2bab1bafa77a83501dd611
Dmitry Shapkin <shapkindev@gmail.com>
2019-02-12 22:42:01 +0300

    GCD Dispatch Source

---
+++ GCD Dispatch Source.playground/Contents.swift
@@ -0,0 +1,14 @@
+// GCD Dispatch Source
+
+import UIKit
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+let timer = DispatchSource.makeTimerSource(queue: DispatchQueue.global())
+timer.setEventHandler {
+    print("!")
+}
+
+timer.schedule(deadline: .now(), repeating: 5)
+timer.activate()
